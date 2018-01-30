057ed891827efcea2c2d88f44e38435504c9563c
Mars Wong <marswong618@gmail.com>
2018-01-30 14:27:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,12 +10,14 @@ class Author
   def add_post(post)
     post.author = self
     @posts << post
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
     @posts << post
+    @@post_count += 1
   end
 
   def post_count
