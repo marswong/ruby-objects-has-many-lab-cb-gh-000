df6a613536bd9f0fa2b2752638767c614e67b9cd
Mars Wong <marswong618@gmail.com>
2018-01-30 14:23:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,12 +10,14 @@ class Artist
   def add_song(song)
     song.artist = @name
     @songs << song
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = @name
     @songs << song
+    @@song_count += 1
   end
 
   def song_count
